147156eee35cd8cadbf204871ae43fc5265c0739
jos <wjosdejong@gmail.com>
2014-07-28 17:14:45 +0200

    Released Timeline v2.9.0

--- js/src/timeline/timeline.js
+++ js/src/timeline/timeline.js
@@ -30,8 +30,8 @@
  * Copyright (c) 2011-2014 Almende B.V.
  *
  * @author  Jos de Jong, <jos@almende.org>
- * @date    2014-05-16
- * @version 2.8.0
+ * @date    2014-07-28
+ * @version 2.9.0
  */
 
 /*
