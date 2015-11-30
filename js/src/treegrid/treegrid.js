38e6600f127bb36cc9af7e2cb5673fd7719f434b
ekarudianto <ekarudianto@yahoo.com>
2015-11-30 15:25:48 +0700

    adding new sortableType parameter, adding new convertion to date if the sortableType is date, remove case sensitive

--- js/src/treegrid/treegrid.js
+++ js/src/treegrid/treegrid.js
@@ -2234,10 +2234,11 @@ links.TreeGrid.Header.prototype.repaint = function () {
                             domField.onclick = function () {
                                 dataConnector.setSorting([{
                                     field: field,
+                                    type: (type) ? type : "string",
                                     order: (order === 'asc') ? 'desc' : (order === 'desc') ? null : 'asc'
                                 }]);
                             }
-                        })(column.name, order)
+                        })(column.name, column.sortableType, order)
                     }
                 }
             }
@@ -4641,11 +4642,12 @@ links.DataTable.prototype._applySortingAndFilters = function () {
         var orders = [];
         for (var f = 0, fMax = this.sorting.length; f < fMax; f++) {
             var entry = this.sorting[f];
-            if (entry.field && entry.order) {
+            if (entry.field && entry.order && entry.type) {
                 var order = entry.order.toLowerCase();
                 if (order == 'asc' || order == 'desc') {
                     orders.push({
                         'field': entry.field,
+                        'type': entry.type,
                         'direction': ((order == 'asc') ? 1 : -1)
                     });
                 }
@@ -4662,9 +4664,12 @@ links.DataTable.prototype._applySortingAndFilters = function () {
                 for (var i = 0; i < len; i++) {
                     var order = orders[i];
                     var field = order.field;
+                    var type = order.type;
                     var direction = order.direction;
+                    var aField = (!a[field]) ? "" : (type == "date") ? new Date(a[field]) : a[field].toLowerCase();
+                    var bField = (!b[field]) ? "" : (type == "date") ? new Date(b[field]) : b[field].toLowerCase();
 
-                    if (a[field] == b[field]) {
+                    if (aField == bField) {
                         if (i == len - 1) {
                             return 0;
                         }
@@ -4673,7 +4678,7 @@ links.DataTable.prototype._applySortingAndFilters = function () {
                         }
                     }
                     else {
-                        return (a[field] > b[field]) ? direction : -direction;
+                        return (aField > bField) ? direction : -direction;
                     }
                 }
             });
